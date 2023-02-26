2e22cd6bfc157069d007baa9dba50dbaa677efe1
Satoutah Haithem <satoutahhaithem@gmail.com>
2023-02-26 23:06:29 +0100

    starting on See all books page and adding recycler view with the adapter

--- app/src/main/java/com/example/managelibrary/AllBooksActivity.java
+++ app/src/main/java/com/example/managelibrary/AllBooksActivity.java
@@ -1,14 +1,28 @@
 package com.example.managelibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class AllBooksActivity extends AppCompatActivity {
+    private RecyclerView booksRecView;
+    private BookRecViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_books);
+
+        booksRecView =findViewById(R.id.booksRecyclerView);
+        adapter=new BookRecViewAdapter(this);
+        booksRecView.setAdapter(adapter);
+        booksRecView.setLayoutManager(new GridLayoutManager(this,2));
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book(1,"Secret","Author",1350,"https://m.media-amazon.com/images/I/81lA9fSZMZL._CR0,204,1224,1224_UX256.jpg","My short Des","My Long desc"));
+        adapter.setBooks(books);
     }
 }
